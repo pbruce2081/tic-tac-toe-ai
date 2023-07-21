80682ab15eeecc59c3f911e6f56d1ff7551919fa
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-21 14:54:59 +0100

    Added post initialisation hoo

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -2,6 +2,7 @@
 
 import enum
 from dataclasses import dataclass
+import re
 
 class Play(str, enum.Enum):
     CROSS = "X"
@@ -18,3 +19,7 @@ class Play(str, enum.Enum):
 class Grid:
     # create the number of cells in the grid
     cells: str = " " * 9
+
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sX0]{9}$", self.cells):
+            raise ValueError("MUST CONTAIN 9 CELLS OF: X, O, OR SPACE")

76d20f4c2daf2e123b76b51b31a0d70b84eeb2b4
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-21 16:15:52 +0100

    Completed all functions to validate

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -4,9 +4,11 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from tic_tac_toe.logic.models import GameState, Grid
+    from tic_tac_toe.logic.models import GameState, Grid, Play
 
 import re
+
+from .exceptions import InvalidGameState
 
 def validate_grid(grid: Grid) -> None:
     if not re.match(r"[\sX0]{9}$", grid.cells):
@@ -20,3 +22,27 @@ def validate_game_state(game_state: GameState) -> None:
 def validate_num_plays(grid: Grid) -> None:
     if abs(grid.x_count - grid.o_count) > 1:
         raise InvalidGameState("WRONG NUMBER OF X'S AND O'S")
+
+def validate_start_plays(grid: Grid, start_play: Play) -> None:
+    if grid.x_count > grid.o_count:
+        if start_play != "X":
+            raise InvalidGameState("WRONG STARTING PLAY")
+    elif grid.o_count > grid.x_count:
+        if start_play != "O":
+            raise InvalidGameState("WRONG STARTING PLAY")
+        
+def validate_winner(grid: Grid, start_play: Play, winner: Play | None) -> None:
+    if winner == "X":
+        if start_play == "X":
+            if grid.x_count <= grid.o_count:
+                raise InvalidGameState("WRONG NUMBER OF X'S")
+        else:
+            if grid.x_count != grid.o_count:
+                raise InvalidGameState("WRONG NUMBER OF X'S")
+    elif winner == "O":
+        if start_play == "O":
+            if grid.o_count <= grid.x_count:
+                raise InvalidGameState("WRONG NUMBER OF O'S")
+        else:
+            if grid.o_count != grid.x_count:
+                raise InvalidGameState("WRONG NUMBER OF O'S")

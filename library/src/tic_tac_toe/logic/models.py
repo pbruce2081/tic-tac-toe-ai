ee019211a32000c5a69c870c9bb577ea5427c847
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-21 14:57:27 +0100

    Added counters for X's, O's and empty cells

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -3,6 +3,7 @@
 import enum
 from dataclasses import dataclass
 import re
+from functools import cached_property
 
 class Play(str, enum.Enum):
     CROSS = "X"
@@ -23,3 +24,18 @@ class Grid:
     def __post_init__(self) -> None:
         if not re.match(r"^[\sX0]{9}$", self.cells):
             raise ValueError("MUST CONTAIN 9 CELLS OF: X, O, OR SPACE")
+    
+    @cached_property
+    # count how many X's there are
+    def x_count(self) -> int:
+        return self.cells.count("X")
+    
+    @cached_property
+    # count how many O's there are
+    def o_count(self) -> int:
+        return self.cells.count("O")
+    
+    @cached_property
+    # count how many empty cells there are
+    def empty_count(self) -> int:
+        return self.cells.count(" ")

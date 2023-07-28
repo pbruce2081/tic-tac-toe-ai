cf14d5e9b3a0e42fb94a3fc3736c7753f664df6d
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-28 15:04:42 +0100

    created function to print the grid

--- frontend/console/frontend-renderers.py
+++ frontend/console/frontend-renderers.py
@@ -1,6 +1,7 @@
 # frontend/console/frontend-renderers.py
 
-
+import textwrap
+from typing import Iterable
 
 from tic_tac_toe.game.renderers import Renderer
 from tic_tac_toe.logic.models import GameState
@@ -14,3 +15,18 @@ def clear_screen() -> None:
 
 def blink(text: str) -> str:
     return f"\033[5m{text}\033[0m"
+
+def print_grid(cells: Iterable[str]) -> None:
+    print(
+        textwrap.dedent(
+            """\
+             A   B   C
+           ------------
+        1 ┆  {0} │ {1} │ {2}
+          ┆ ───┼───┼───
+        2 ┆  {3} │ {4} │ {5}
+          ┆ ───┼───┼───
+        3 ┆  {6} │ {7} │ {8}
+    """
+        ).format(*cells)
+    )

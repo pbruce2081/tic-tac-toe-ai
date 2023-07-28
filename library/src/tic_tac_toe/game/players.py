6155f10bf97a7dcb85c8f62e2bf594043dd34176
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-28 14:25:08 +0100

    Commiting all files

--- library/src/tic_tac_toe/game/players.py
+++ library/src/tic_tac_toe/game/players.py
@@ -8,3 +8,8 @@ from logic.models import GameState, Play, Move
 class Player(metaclass=abc.ABCMeta):
     def __init__(self, play: Play) -> None:
         self.play = play
+
+    def make_move(self, game_state: GameState) -> GameState:
+        if self.play is game_state.curr_play:
+            if move:
+                move = self.get_move(game_state):

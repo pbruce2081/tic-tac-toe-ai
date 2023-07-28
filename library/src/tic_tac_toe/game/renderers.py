35898f0a36b40bd81cb483f0b74747df1bbc4232
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-28 14:47:03 +0100

    Created function to render game states

--- library/src/tic_tac_toe/game/renderers.py
+++ library/src/tic_tac_toe/game/renderers.py
@@ -5,4 +5,6 @@ import abc
 from tic_tac_toe.logic.models import GameState
 
 class Renderer(metaclass=abc.ABCMeta):
-    
+    @abc.abstractmethod
+    def render(self, game_state: GameState) -> None:
+        """RENDER THE CURRENT GAME STATE"""

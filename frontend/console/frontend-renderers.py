04140a995cb78decae0dc2307b378053c8097eee
Phoebe Bruce <pbruce2081@gmail.com>
2023-07-28 14:58:11 +0100

    Added function to make text blink

--- frontend/console/frontend-renderers.py
+++ frontend/console/frontend-renderers.py
@@ -1,5 +1,7 @@
 # frontend/console/frontend-renderers.py
 
+
+
 from tic_tac_toe.game.renderers import Renderer
 from tic_tac_toe.logic.models import GameState
 
@@ -9,3 +11,6 @@ class ConsoleRenderer(Renderer):
     
 def clear_screen() -> None:
     print("\033c", end="")
+
+def blink(text: str) -> str:
+    return f"\033[5m{text}\033[0m"
